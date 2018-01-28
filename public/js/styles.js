a65957468e9b18ca1ff3e83ff6a6ba0ce438e1a4
courtney <coa.mcallister@gmail.com>
2018-01-28 16:12:58 -0500

    add locations, styles, and subjects files

---
+++ public/js/styles.js
@@ -0,0 +1,20 @@
+module.exports = [
+  "american traditional",
+  "realism",
+  "watercolor",
+  "tribal (polynesian)",
+  "new school",
+  "neo-traditional",
+  "traditional japanese",
+  "blackwork",
+  "illustrative",
+  "biomechanical",
+  "color horror",
+  "neo-japanese",
+  "surrealism",
+  "trash polka",
+  "dotwork (pointalism)",
+  "geometric",
+  "stick and poke",
+  "sketchwork"
+];
---
+++ public/js/subjects.js
@@ -0,0 +1,19 @@
+module.exports = [
+  "butterfly",
+  "ninja",
+  "arrow",
+  "infinity sign",
+  "golden spiral",
+  "succulent",
+  "dragonfly",
+  "coffee cup",
+  "skeleton",
+  "feather",
+  "dreamcatcher",
+  "chinese symbol that means white hooker",
+  "moon",
+  "sun",
+  "sword",
+  "skull",
+  "rose"
+];
