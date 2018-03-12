52c3a2a3e33e85518146edcbd1b09a72095709d6
courtney <coa.mcallister@gmail.com>
2018-03-11 20:18:15 -0400

    add watchify build script. Random tat is generated on load, not onclick unfortunately

--- public/bundle.js
+++ public/bundle.js
@@ -3,12 +3,17 @@ const locations = require('./locations');
 const styles = require('./styles');
 const subjects = require('./subjects');
 
-
+var randomLocationNum = Math.floor(Math.random() * (locations.length))
+var randomStyleNum = Math.floor(Math.random() * (styles.length))
+var randomSubjectNum = Math.floor(Math.random() * (subjects.length))
+var tatLocation = locations[randomLocationNum]
+var tatStyle = styles[randomStyleNum]
+var tatSubject = subjects[randomSubjectNum]
+document.getElementById('tatDisplay').innerHTML = `You're getting a ${tatStyle} ${tatSubject} on your ${tatLocation}! Congrats!`
 
 function generate(){
-  var randomNum = Math.floor(Math.random() * (locations.length))
-  document.getElementById('tatDisplay').innerHTML = locations[randomNum]
-  console.log('hey')
+  document.getElementById('tatDisplay').innerHTML = tatLocation
+  // console.log('hey');
 }
 
 },{"./locations":2,"./styles":3,"./subjects":4}],2:[function(require,module,exports){
--- public/js/app.js
+++ public/js/app.js
@@ -2,10 +2,15 @@ const locations = require('./locations');
 const styles = require('./styles');
 const subjects = require('./subjects');
 
-
+var randomLocationNum = Math.floor(Math.random() * (locations.length))
+var randomStyleNum = Math.floor(Math.random() * (styles.length))
+var randomSubjectNum = Math.floor(Math.random() * (subjects.length))
+var tatLocation = locations[randomLocationNum]
+var tatStyle = styles[randomStyleNum]
+var tatSubject = subjects[randomSubjectNum]
+document.getElementById('tatDisplay').innerHTML = `You're getting a ${tatStyle} ${tatSubject} on your ${tatLocation}! Congrats!`
 
 function generate(){
-  var randomNum = Math.floor(Math.random() * (locations.length))
-  document.getElementById('tatDisplay').innerHTML = locations[randomNum]
-  console.log('hey')
+  document.getElementById('tatDisplay').innerHTML = tatLocation
+  // console.log('hey');
 }
