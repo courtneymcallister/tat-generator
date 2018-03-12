bd97bdc9299c5319e492dcb71546b5cdd080c61f
courtney <coa.mcallister@gmail.com>
2018-03-12 19:50:32 -0400

    add code to set the indefinite article before tat style output

--- public/bundle.js
+++ public/bundle.js
@@ -3,24 +3,32 @@ const locations = require('./locations');
 const styles = require('./styles');
 const subjects = require('./subjects');
 
+const vowels = ['a', 'e', 'i', 'o', 'u'] //lol
+
 function generateTat(){
+
+  //generate random index number in each list
   var randomLocationNum = Math.floor(Math.random() * (locations.length))
   var randomStyleNum = Math.floor(Math.random() * (styles.length))
   var randomSubjectNum = Math.floor(Math.random() * (subjects.length))
+  //choose each part at generated index
   var tatLocation = locations[randomLocationNum]
   var tatStyle = styles[randomStyleNum]
   var tatSubject = subjects[randomSubjectNum]
-  document.getElementById('tatDisplay').innerHTML = `You're getting a ${tatStyle} ${tatSubject} on your ${tatLocation}! Congrats!`
+
+  //set indefinite article to 'a' or 'an'
+  if (vowels.indexOf(tatStyle.substring(0,1)) >= 0){
+    var article = 'an'
+  } else {
+    var article = 'a'
+  }
+
+  //spit out full string
+  document.getElementById('tatDisplay').innerHTML = `You're getting ${article} ${tatStyle} ${tatSubject} on your ${tatLocation}! Congrats!`
 }
 
 var generate = document.getElementById('generate')
 generate.addEventListener('click', generateTat)
-
-
-// function generate(){
-//   document.getElementById('tatDisplay').innerHTML = tatLocation
-//   // console.log('hey');
-// }
 
 },{"./locations":2,"./styles":3,"./subjects":4}],2:[function(require,module,exports){
 module.exports = [
