78aecc8ecda4aaf0760a36d549c48ec54e2c722c
courtney <coa.mcallister@gmail.com>
2018-03-11 20:40:46 -0400

    add event listener to button. Tat now generates onclick

--- public/bundle.js
+++ public/bundle.js
@@ -3,18 +3,24 @@ const locations = require('./locations');
 const styles = require('./styles');
 const subjects = require('./subjects');
 
-var randomLocationNum = Math.floor(Math.random() * (locations.length))
-var randomStyleNum = Math.floor(Math.random() * (styles.length))
-var randomSubjectNum = Math.floor(Math.random() * (subjects.length))
-var tatLocation = locations[randomLocationNum]
-var tatStyle = styles[randomStyleNum]
-var tatSubject = subjects[randomSubjectNum]
-document.getElementById('tatDisplay').innerHTML = `You're getting a ${tatStyle} ${tatSubject} on your ${tatLocation}! Congrats!`
-
-function generate(){
-  document.getElementById('tatDisplay').innerHTML = tatLocation
-  // console.log('hey');
+function generateTat(){
+  var randomLocationNum = Math.floor(Math.random() * (locations.length))
+  var randomStyleNum = Math.floor(Math.random() * (styles.length))
+  var randomSubjectNum = Math.floor(Math.random() * (subjects.length))
+  var tatLocation = locations[randomLocationNum]
+  var tatStyle = styles[randomStyleNum]
+  var tatSubject = subjects[randomSubjectNum]
+  document.getElementById('tatDisplay').innerHTML = `You're getting a ${tatStyle} ${tatSubject} on your ${tatLocation}! Congrats!`
 }
+
+var generate = document.getElementById('generate')
+generate.addEventListener('click', generateTat)
+
+
+// function generate(){
+//   document.getElementById('tatDisplay').innerHTML = tatLocation
+//   // console.log('hey');
+// }
 
 },{"./locations":2,"./styles":3,"./subjects":4}],2:[function(require,module,exports){
 module.exports = [
